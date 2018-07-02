80bc442efa3e74daa45b947392e95cdc5c6f1f1b
Gang-Ryung Uh <gangryunguh@gmail.com>
2018-07-02 13:01:51 -0500

    Pr7-22 const array compilation issue is fixed.

--- app/Pr7-22.cpp
+++ app/Pr7-22.cpp
@@ -13,18 +13,18 @@ void showArray(const int [][COLS], int); // Function prototype
 
 int main()
 {
-   int table1[TBL1_ROWS][COLS] = {{1, 2, 3, 4},
+   const int table1[TBL1_ROWS][COLS] = {{1, 2, 3, 4},
                                   {5, 6, 7, 8},
                                   {9, 10, 11, 12}};
-   int table2[TBL2_ROWS][COLS] = {{10, 20, 30, 40},
+   const int table2[TBL2_ROWS][COLS] = {{10, 20, 30, 40},
                                   {50, 60, 70, 80},
                                   {90, 100, 110, 120},
                                   {130, 140, 150, 160}};
                                   
    cout << "The contents of table1 are:\n";
-   showArray((const int[][COLS])table1, TBL1_ROWS);
+   showArray(table1, TBL1_ROWS);
    cout << "The contents of table2 are:\n";
-   showArray((const int[][COLS])table2, TBL2_ROWS);
+   showArray(table2, TBL2_ROWS);
    return 0;
 }
 
